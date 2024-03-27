d4435d159e561a2b93e78dcf3d78347d98fce3b4
Jafar Janshanlo <jafar.janshanlo20@gmail.com>
2024-03-27 10:52:13 +0300

    Initial Commit

---
+++ ChatAppJJ/ChatAppJJApp.swift
@@ -0,0 +1,17 @@
+//
+//  ChatAppJJApp.swift
+//  ChatAppJJ
+//
+//  Created by Jafar on 27.03.2024.
+//
+
+import SwiftUI
+
+@main
+struct ChatAppJJApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
