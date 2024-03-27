10372fde1511f0090f50868c862adbffd1ea870d
Jafar Janshanlo <jafar.janshanlo20@gmail.com>
2024-03-28 00:11:27 +0300

    Add Firebase SDK and inizialization Code

--- ChatAppJJ/ChatAppJJApp.swift
+++ ChatAppJJ/ChatAppJJApp.swift
@@ -6,10 +6,17 @@
 //
 
 import SwiftUI
+import Firebase
 
 @main
 struct ChatAppJJApp: App {
+    
+    init() {
+        FirebaseApp.configure()
+    }
+    
     var body: some Scene {
+        
         WindowGroup {
             ContentView()
         }
